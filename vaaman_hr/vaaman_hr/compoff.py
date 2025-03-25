7dcb9b2f358ce9ab6f024e6297aaff86643b05bd
ptpratul2 <ptpratul2@gmail.com>
2025-03-25 13:51:43 +0530

    Update compoff.py

--- vaaman_hr/vaaman_hr/compoff.py
+++ vaaman_hr/vaaman_hr/compoff.py
@@ -16,33 +16,6 @@ from hrms.hr.utils import (
 )
 
 class CompOff(CompensatoryLeaveRequest): 
-    def validate_attendance(self):
-        try:
-            attendance_records = frappe.get_all(
-                "Attendance",
-                filters={
-                    "attendance_date": ["between", (self.work_from_date, self.work_end_date)],
-                    "status": ("in", ["Present", "Work From Home", "Half Day","Weekly Off"]),
-                    "docstatus": 1,
-                    "employee": self.employee,
-                },
-                fields=["attendance_date", "status"],
-            )
-            
-            half_days = [entry.attendance_date for entry in attendance_records if entry.status == "Half Day"]
-
-            if half_days and (not self.half_day or getdate(self.half_day_date) not in half_days):
-                frappe.throw(
-                    _(f"You were only present for Half Day on {', '.join([frappe.bold(format_date(half_day)) for half_day in half_days])}. Cannot apply for a full day compensatory leave")
-                )
-
-            if len(attendance_records) < date_diff(self.work_end_date, self.work_from_date) + 1:
-                frappe.throw(_("You are not present all day(s) between compensatory leave request days"))
-        
-        except Exception as e:
-            frappe.log_error(f"Error in validate_attendance: {str(e)}", "Validate Attendance Error")
-            frappe.throw(_("An unexpected error occurred. Please check the error log for more details."))
-
     def validate_holidays(self):
         try:
             frappe.logger().info(f"Starting validate_holidays for Employee: {self.employee}")
@@ -91,12 +64,19 @@ class CompOff(CompensatoryLeaveRequest):
             wfh = [entry.attendance_date for entry in attendance_records if entry.status == "Work From Home"]
             weekend_days = [entry["attendance_date"] for entry in weekend if entry["weekly_off"] == 1]
 
-            frappe.logger().info(f"Overtime Days: {overtime_days}")
-            frappe.logger().info(f"Work From Home Days: {wfh}")
-            frappe.logger().info(f"Weekend Days: {weekend_days}")
+            all_dates = list(set(overtime_days + holidays + wfh + weekend_days))
+            all_dates.sort()
 
-            if holidays or overtime_days or weekend_days or wfh:
-		frappe.msgprint("Compensatory leave will be added for the following dates: {}".format(", ".join([frappe.bold(format_date(day)) for day in overtime_days or holidays or wfh or weekend_days])))
+            if all_dates:
+                if len(all_dates) == 1:
+                    frappe.msgprint("Compensatory leave will be added for the date: {}".format(
+                        frappe.bold(format_date(all_dates[0]))
+                    ))
+                else:
+                    frappe.msgprint("Compensatory leave will be added from {} to {}".format(
+                        frappe.bold(format_date(min(all_dates))),
+                        frappe.bold(format_date(max(all_dates)))
+                    ))
             else:
                 msg = _(f"The days between {format_date(self.work_from_date)} to {format_date(self.work_end_date)} are not valid holidays or weekly offs with overtime.")
                 frappe.throw(msg)
