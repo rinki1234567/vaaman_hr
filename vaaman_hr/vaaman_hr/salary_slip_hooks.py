9ed553115d66eaf2691ccf95baff886fb392a989
ptpratul2 <ptpratul2@gmail.com>
2026-05-13 11:58:32 +0530

    Fetch total overtime from Overtime Import Item

--- vaaman_hr/vaaman_hr/salary_slip_hooks.py
+++ vaaman_hr/vaaman_hr/salary_slip_hooks.py
@@ -3,15 +3,17 @@ import frappe
 def calculate_overtime_hours(doc, method):
     if not doc.start_date or not doc.end_date:
         return
-    
-    # Fetch total overtime hours from Attendance
+
+    # Fetch total overtime hours from Overtime Import Item
     total_overtime = frappe.db.sql("""
-        SELECT SUM(custom_over_time) 
-        FROM `tabAttendance`
-        WHERE employee = %s 
-        AND attendance_date BETWEEN %s AND %s
-        AND docstatus = 1
+        SELECT SUM(oti.over_time)
+        FROM `tabOvertime Import Item` oti
+        INNER JOIN `tabOverTime Import` oti_parent
+            ON oti.parent = oti_parent.name
+        WHERE oti.employee = %s
+        AND oti.attendance_date BETWEEN %s AND %s
+        AND oti_parent.docstatus = 1
     """, (doc.employee, doc.start_date, doc.end_date))[0][0] or 0
-    
-    # Update the total_overtime_hours field in Salary Slip
+
+    # Update Salary Slip field
     doc.total_overtime_hours = total_overtime
