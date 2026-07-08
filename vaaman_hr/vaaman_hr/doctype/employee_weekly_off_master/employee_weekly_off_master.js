33eb9c10c2b6423ea5240ebee7e9a3e5613c1ee1
rinki1234567 <rinkijatav24@navgurukul.org>
2026-07-08 14:03:56 +0530

    Add Employee Weekly Off Master and Employee weekly off days field in salary slip

---
+++ vaaman_hr/vaaman_hr/doctype/employee_weekly_off_master/employee_weekly_off_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Pratul Tripathi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Employee Weekly Off Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
